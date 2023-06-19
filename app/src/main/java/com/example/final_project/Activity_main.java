c8fc059981d09d942d0f6c3e02f2f87e4961cbb3
Ramon Matute <ramonmatute2003@gmail.com>
2023-06-18 22:26:10 -0600

    Initial commit: 2023/06/18 22:27

--- app/src/main/java/com/example/final_project/Activity_main.java
+++ app/src/main/java/com/example/final_project/Activity_main.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Activity_main extends AppCompatActivity {
 
+    //prueba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
