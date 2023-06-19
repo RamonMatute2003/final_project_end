a10c60f9833830dadd50375afd54889c8cd7d212
Usuario <fabriciojaviermoreno3.99@gmail.com>
2023-06-18 22:29:56 -0600

    prueba

--- app/src/main/java/com/example/final_project/Activity_main.java
+++ app/src/main/java/com/example/final_project/Activity_main.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 public class Activity_main extends AppCompatActivity {
 
     //prueba
+
+
+    //prueba 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
