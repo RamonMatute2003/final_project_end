4fe71a1fa4f14e8297d3412c8db4c8e1b5b8e562
Ramon Matute <ramonmatute2003@gmail.com>
2023-06-25 20:01:18 -0600

    Initial commit: 2023/06/25 21:14 Diseño de activitis sign_in y welcome

--- app/src/main/java/com/example/final_project/Activity_sign_in.java
+++ app/src/main/java/com/example/final_project/Activity_sign_in.java
@@ -1,31 +1,49 @@
 package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Options;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.ImageViewTarget;
+
 
 public class Activity_sign_in extends AppCompatActivity {
 
     EditText txt_email, txt_password;//email=correo, password=contraseña
     Button btn_sign_in, btn_login;//sign_in=registrarse, login=iniciar sesion
+    ImageView image_view;
+    Uri svg_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        image_view=findViewById(R.id.imageView4);
+        svg_uri=Uri.parse("android.resource://" + getPackageName() + "/drawable/image_login");
+
         txt_email=(EditText) findViewById(R.id.txt_email);
         txt_password=(EditText) findViewById(R.id.txt_password);
-        btn_login=(Button) findViewById(R.id.btn_login);
-        //btn_sign_in=(Button) findViewById(R.id.btn_sign_in);
+        btn_sign_in=(Button) findViewById(R.id.btn_sign_in);
 
-        if(txt_email.getText().toString().isEmpty() || txt_password.getText().toString().isEmpty()){
-            Toast.makeText(getApplicationContext(), "No dejar campos vacios", Toast.LENGTH_LONG).show();
-        }else{
+        adapt_image();
+    }
 
-        }
+    private void adapt_image(){
+        //Glide.with(this).load(R.drawable.image_login).into(image_view);
     }
 }
